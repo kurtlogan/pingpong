487a5f6b03c98e131981c083142d343734e0abb2
Kurt Logan <kurt.logan@hotmail.co.uk>
2017-05-23 20:03:48 +0100

    Added docker files

--- ping/index.js
+++ ping/index.js
@@ -1,15 +1,22 @@
 const fetch = require("node-fetch")
 const express = require("express")
 const app = express()
+const port = parseInt(process.argv[2])
+
+if (isNaN(port)) {
+  throw `Port ${process.argv[2]} is not valid`
+}
 
 app.get('/', (req, res) => {
   console.log("ping")
 
   setTimeout(() => {
-    fetch("http://pong").then(() => { res.send("done") })
+    fetch("http://pong:3001")
+      .then(() => res.send("done"))
+      .catch(() => res.send("failed"))
   }, 1000)
 })
 
-app.listen(3000, () => {
-  console.log("Ping started on port 3000!")
+app.listen(port, () => {
+  console.log(`Ping started on port ${port}!`)
 })
--- pong/index.js
+++ pong/index.js
@@ -1,15 +1,22 @@
 const fetch = require("node-fetch")
 const express = require("express")
 const app = express()
+const port = parseInt(process.argv[2])
+
+if (isNaN(port)) {
+  throw `Port ${process.argv[2]} is not valid`
+}
 
 app.get('/', (req, res) => {
   console.log("pong")
 
   setTimeout(() => {
-    fetch("http://ping").then(() => { res.send("done") })
+    fetch("http://ping:3000")
+      .then(() => res.send("done"))
+      .catch(() => res.send("failed"))
   }, 1000)
 })
 
-app.listen(3000, () => {
-  console.log("Pong started on port 3000!")
+app.listen(port, () => {
+  console.log(`Pong started on port ${port}!`)
 })
